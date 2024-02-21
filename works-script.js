8a38ee6cd1e01c19677ea3232ef1d729b9b2f8c4
Piyush Maan <psmaan444@gmail.com>
2024-02-21 11:08:52 +0530

    works

---
+++ works-script.js
@@ -0,0 +1,9 @@
+function hideWebsites() {
+    document.querySelector(".uiux").style.display = "none";
+    document.querySelector(".websites").style.display = "flex";
+}
+
+function hideUIUX() {
+    document.querySelector(".websites").style.display = "none";
+    document.querySelector(".uiux").style.display = "grid";
+}
